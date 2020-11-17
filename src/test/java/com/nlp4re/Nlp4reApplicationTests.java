182bb8201ac177fad669c2dd74b500341e30c2c5
yenle2501 <leyen.coburg@gmail.com>
2020-11-17 13:01:48 +0100

    init

---
+++ src/test/java/com/nlp4re/Nlp4reApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nlp4re;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nlp4reApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
